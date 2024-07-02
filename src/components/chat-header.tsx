086eb4a4d95c16aae5d6cb9fa81692a33b74f8c8
Yashwant <yashwant08012005@gmail.com>
2024-07-02 21:13:59 +0530

    added group profile

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -2,6 +2,10 @@ import { useState,useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
 import {EditIcon,BackIcon,VerticalDotsIcon,ShareNumberIcon,MembersIcon,ReportIcon} from "../icons.tsx";
 import {Data} from './types.ts'
+import Displaybl from '../assets/Displaybl.png'
+import Displaybr from '../assets/Displaybr.png'
+import Displaytl from '../assets/Displaytl.png'
+import Displaytr from '../assets/Displaytr.png'
 interface chatHeader{
     from:string,
     to:string,
@@ -28,7 +32,7 @@ export default function ChatHeader (){
       },[])
   return (
     
-    <div className = 'fixed top-0 h-28 z-10 w-screen border-b-2 border-[#E5E5E0] flex flex-col justify-between px-5 py-4 text-[#141e0d] space-y-1 bg-[#faf9f7]'>
+    <div className = 'fixed top-0 h-28 z-10 w-screen border-b-2 border-[#E5E5E0] flex flex-col justify-center px-4  text-[#141e0d] space-y-1 bg-[#faf9f7]'>
         <div className = 'flex justify-between items-center'>
             <div className = 'space-x-3 flex items-center'>
                 <BackIcon/>
@@ -37,10 +41,18 @@ export default function ChatHeader (){
             <EditIcon/>
         </div>
         <div className = 'flex justify-between items-center'>
-            <p className = 'text-nowrap'>
-                <span className = 'text-[#606060] mulish-500 text-base'>From </span><span className = 'mulish-700 text-lg text-[#141e0d] font-extrabold'>{chatHeader.from}</span><br />
-                <span className = 'text-[#606060] mulish-500 text-base'>To </span><span className = 'mulish-700 text-lg text-[#141e0d] font-extrabold'>{chatHeader.to}</span>
-            </p> 
+            <div className = 'flex gap-6 items-center'>
+                <div className = 'relative size-12 overflow-hidden rounded-full'>
+                    <div className = 'absolute top-0 left-0 size-6'><img src={Displaytl} alt="display" /></div>
+                    <div className = 'absolute bottom-0 left-0 size-6'><img src={Displaybl} alt="display" /></div>
+                    <div className = 'absolute top-0 right-0 size-6'><img src={Displaytr} alt="display" /></div>
+                    <div className = 'absolute bottom-0 right-0 size-6'><img src={Displaybr} alt="display" /></div>
+                </div>
+            <div className = 'grid grid-rows-2 -space-y-3 items-center'>
+                <div><span className = 'text-[#606060] mulish-500 text-base'>From </span><span className = 'mulish-700 text-lg text-[#141e0d] font-extrabold'>{chatHeader.from}</span></div>
+                <div><span className = 'text-[#606060] mulish-500 text-base'>To </span><span className = 'mulish-700 text-lg text-[#141e0d] font-extrabold'>{chatHeader.to}</span></div>
+            </div> 
+            </div>
             <button className = 'relative' onClick={()=>{setOpen((prevState)=>!prevState)}}><VerticalDotsIcon/>
                 {open && <div className = 'absolute top-6 right-0 w-40 h-36 grid grid-rows-3 px-2 bg-white divide-y divide-[#E5E5E0] rounded-lg drop-shadow-md'>
                     <button className = 'flex gap-2 items-center'>
